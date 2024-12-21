c6b7cdb54536a14656e8fd058901b7a877ebec6f
Nir Gluzman <nir.gluzman@gmail.com>
2024-12-21 22:33:58 +0100

    create basic template for Focus

---
+++ src/features/Focus.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { colors } from '../utils/colors';
+
+export const Focus = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Focus Feature</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  text: {
+    color: colors.white,
+  },
+});
